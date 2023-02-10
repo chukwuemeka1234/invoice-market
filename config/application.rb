2e7acfe73502253740492e5d498c4e29fe4a7998
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2023-02-10 19:53:02 +0100

    Setting up cors

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,17 @@ module InvoiceMarket
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # config/initializers/cors.rb
+
+    Rails.application.config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource(
+          '*',
+          headers: :any, methods: [:get, :post, :patch, :delete, :put, :options]
+        )
+      end
+    end
   end
 end
